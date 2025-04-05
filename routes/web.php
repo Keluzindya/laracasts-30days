716bd574f3590d982895ed0921700810d1ad439c
Keluzindya <lakerenluzindya@gmail.com>
2025-04-04 21:58:55 -0500

    Day-16

--- routes/web.php
+++ routes/web.php
@@ -9,25 +9,45 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     //eager loading
-    $jobs = Job::with('employer')->simplePaginate(3);
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+
+    return view('jobs/index', [
         'jobs' => $jobs
     ]);
 });
 
-Route::get('/jobs-test', function () {
-    $jobs = Job::all();
-//    dd($jobs);
-//    dd($jobs[0]);
-    dd($jobs[0]->title . ' ' . $jobs[0]->salary);
+//create a job
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
+
+
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    // validation...
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
     return view('contact');
 });
+
+//Route::get('/jobs-test', function () {
+//    $jobs = Job::all();
+////    dd($jobs);
+////    dd($jobs[0]);
+//    dd($jobs[0]->title . ' ' . $jobs[0]->salary);
+//});
