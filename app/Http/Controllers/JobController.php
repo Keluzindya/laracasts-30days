63cff9ea263e281da9289e1e60f10955d8120662
Keluzindya <lakerenluzindya@gmail.com>
2025-05-13 20:47:31 -0500

    Day-24

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
+
 class JobController extends Controller
 {
     public function index()
@@ -30,18 +33,22 @@ class JobController extends Controller
 
     public function store()
     {
-        // getb auth user
+        // get auth user
         // get employer with matching user id
         request()->validate([
             'title' => ['required', 'min:3'],
             'salary' => ['required']
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1 // use the employer id
         ]);
+
+        Mail::to($job->employer->user)->send(
+            new JobPosted($job)
+        );
 
         return redirect('/jobs');
     }
